a91a9780b0e5a5e3068fa0dcee42598f46dac76f
keshuook <amritanshuanita@gmail.com>
2020-06-10 12:09:39 +0530

    finaly your index is done

---
+++ json.js
@@ -0,0 +1,7 @@
+var json = {
+	'link1': '',
+	'link2': 'music',
+	'link3': 'software',
+	'link4': 'contact',
+	'link5': 'game'
+};
---
+++ script.js
@@ -0,0 +1,37 @@
+window.onscroll = function() {
+	isSticky()
+};
+
+var head = document.getElementById("top-nav");
+var sticky = head.offsetTop;
+
+function isSticky() {
+  if (window.pageYOffset > sticky) {
+    head.classList.add("sticky");
+  } else {
+    head.classList.remove("sticky");
+  }
+}
+function leave(x){
+	document.getElementById('iframe').style.display = "block";
+	setTimeout(function(){
+		window.location.href = x;
+	},800);
+}
+function feed(x){
+	if(x == 'like'){
+		localStorage.setItem('feed','like');
+	}else if (x == 'dis i like'){
+		localStorage.setItem('feed','dis i like');		
+	}
+}
+setInterval(function(){
+var feed = localStorage.getItem('feed');
+if (feed == "like"){
+	document.getElementById('like').style.backgroundColor = "red";
+	document.getElementById('dis i like').style.backgroundColor = "";
+}else if (feed == 'dis i like'){
+	document.getElementById('dis i like').style.backgroundColor = "red";
+	document.getElementById('like').style.backgroundColor = "";
+}
+},50);
