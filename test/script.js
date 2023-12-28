7eb283c7d08a0ae058145742eca043f243ca39e2
Keshuook <59391084+keshuook@users.noreply.github.com>
2023-12-28 15:55:01 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -7,7 +7,7 @@ var scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-// renderer.setClearColor(0xffffff);
+renderer.setClearColor(0x4F565E);
 
 // Fix the screen if the user resizes.
 window.addEventListener("resize", () => {
